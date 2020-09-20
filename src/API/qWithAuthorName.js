2149bf3508e4c1d0ad846cbe263f9a900f11b721
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-20 12:59:08 +0300

    Add API queryWithAuthorName

---
+++ src/API/qWithAuthorName.js
@@ -0,0 +1,19 @@
+import { parseString } from 'xml2js';
+import { BASE_URL, GOODREADS_API_KEY } from '../constants';
+
+const authorName = 'Dostoyevski';
+const endpoint = new URL(`${BASE_URL}/author_url/${authorName}?key=${GOODREADS_API_KEY}`);
+
+fetch(endpoint, {
+  mode: 'cors',
+})
+  .then(
+    response => response.text(),
+  )
+  .then(
+    response => {
+      parseString(response, (err, res) => {
+        // do something with res
+      });
+    },
+  );
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './API';
 
 ReactDOM.render(
   <h1>Hello React!</h1>,
