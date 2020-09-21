eb8e50922b4971528da785a5aa44e3d55a2c8764
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-21 20:51:57 +0300

    Add env variable option

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
-export const GOODREADS_API_KEY = process.env.REACT_APP_GOODREADS_API_KEY;
+export const GOODREADS_API_KEY = process.env.REACT_APP_GOODREADS_API_KEY
+  || process.env.GOODREADS_API_KEY;
 export const BASE_URL = 'https://cors-anywhere.herokuapp.com/https://www.goodreads.com/';
 
 export const WELCOME = 'WELCOME';
