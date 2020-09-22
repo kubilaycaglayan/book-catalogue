0d65cbb8c8023a0e507bef7c5cad22517cacf2cb
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-22 16:31:27 +0300

    Add goodreads logo

--- src/components/QueryPage.js
+++ src/components/QueryPage.js
@@ -12,8 +12,19 @@ const QueryPage = () => (
       <i className="fas fa-book" />
       <i className="fas fa-user-friends" />
     </div>
+    <h2 className="mt-4 ml-3">
+      Powered by
+      <a href="https://www.goodreads.com/" target="_blank" rel="noreferrer">
+        <img alt="goodreads logo" className="d-block" src="https://s.gr-assets.com/assets/home/header_logo-8d96d7078a3d63f9f31d92282fd67cf4.png" />
+      </a>
+    </h2>
     <h2>
-      Powered by Goodreads
+      Created by
+      <p>
+        <a href="https://kubilaycaglayan.com" target="_blank" rel="noreferrer">
+          Kubilay Caglayan
+        </a>
+      </p>
     </h2>
   </div>
 );
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,7 +5,7 @@ const Welcome = () => (
   <div className="welcome-page">
     <h1>
       <i className="fas fa-book-open" />
-      Welcome to the books catalogue
+      Welcome to the book catalogue
     </h1>
     <Link to="/query" className="main-page-link">
       <i className="fas fa-search" />
