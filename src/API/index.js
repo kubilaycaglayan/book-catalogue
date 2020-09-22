b5e1bf44b4b9617d8b81a60706626f108d8a2665
kubilaycaglayan <kblycaglayan@gmail.com>
2020-09-22 15:39:07 +0300

    Update .gitignore

--- src/API/index.js
+++ src/API/index.js
@@ -1,9 +1,3 @@
 export { default as autoComplete } from './autoComplete';
 export { default as getAuthor } from './getAuthor';
 export { default as getBook } from './getBook';
-
-/* export {
-  results as autoComplete,
-  author as getAuthor,
-  book as getBook,
-} from './fakeObjects'; */
